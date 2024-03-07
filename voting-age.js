314346140f56fd3bba893b836a123be0c09f63c1
meredithGatlin <mgarcia91@student.cccd.edu>
2024-03-07 10:36:35 -0800

    updated with profs feedback

--- voting-age.js
+++ voting-age.js
@@ -5,12 +5,7 @@ function checkVotingAge(age) {
   // TODO: Implement this function.
   let result;
   let age = 18;
-  if (age >= 18) {
-    return 'true';
-  } else {
-    return 'false';
-  }
-  return result;
+  return age >=18;
 }
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
